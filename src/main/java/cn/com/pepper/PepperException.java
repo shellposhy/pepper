42b4c51139bcac542afdd61989cb9db4f954da58
shishb <shishb@pku.edu.cn>
2017-07-12 17:42:53 +0800

    update PepperException

--- src/main/java/cn/com/pepper/PepperException.java
+++ src/main/java/cn/com/pepper/PepperException.java
@@ -1,9 +1,5 @@
 package cn.com.pepper;
 
-import java.io.IOException;
-import org.apache.lucene.index.CorruptIndexException;
-import org.apache.lucene.store.LockObtainFailedException;
-
 /**
  * Lucene Indexing Exception
  * 
@@ -11,11 +7,7 @@ import org.apache.lucene.store.LockObtainFailedException;
  * @version 1.0
  */
 public class PepperException extends Exception {
-	public static final int CAUSE_TYPE_Exception = 10;
-	public static final int CAUSE_TYPE_LockObtainFailedException = 11;
-	public static final int CAUSE_TYPE_IOException = 12;
-	public static final int CAUSE_TYPE_CorruptIndexException = 13;
-	private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = -4977944640525893593L;
 
 	public PepperException(String message, Throwable throwable) {
 		super(message, throwable);
@@ -25,19 +17,4 @@ public class PepperException extends Exception {
 		super(message);
 	}
 
-	public int getCauseType() {
-		Throwable throwable = getCause();
-		if (throwable != null) {
-			if ((throwable instanceof LockObtainFailedException)) {
-				return 11;
-			}
-			if ((throwable instanceof IOException)) {
-				return 12;
-			}
-			if ((throwable instanceof CorruptIndexException)) {
-				return 13;
-			}
-		}
-		return 10;
-	}
 }
