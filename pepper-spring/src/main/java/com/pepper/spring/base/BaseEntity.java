c44730eb421439c9ea06b46ce2201c6cb5828267
shishaobo <shishaobo@zgshfp.com.cn>
2019-01-15 15:26:01 +0800

    update pom and extra add BaseEntity new fields

--- pepper-spring/src/main/java/com/pepper/spring/base/BaseEntity.java
+++ pepper-spring/src/main/java/com/pepper/spring/base/BaseEntity.java
@@ -1,11 +1,17 @@
 package com.pepper.spring.base;
 
 import java.io.Serializable;
+import java.util.Date;
 
 import org.apache.lucene.document.Field;
 
 import com.pepper.spring.annotation.PepperField;
 import com.pepper.spring.enums.EDataType;
+
+import static cn.com.lemon.base.Strings.uuid;
+import static cn.com.lemon.base.DateUtil.year;
+import static cn.com.lemon.base.DateUtil.month;
+import static cn.com.lemon.base.DateUtil.day;;
 
 /**
  * Lucene base Model
@@ -15,8 +21,16 @@ import com.pepper.spring.enums.EDataType;
  */
 public class BaseEntity implements Serializable {
 	private static final long serialVersionUID = 6549885313472962978L;
-	@PepperField(key = "UUID", store = Field.Store.YES, index = Field.Index.NOT_ANALYZED, termVector = Field.TermVector.NO, type = EDataType.STRING)
-	private String uuid;
+	@PepperField(key = "UUID", store = Field.Store.YES, index = Field.Index.NOT_ANALYZED, type = EDataType.STRING)
+	private String uuid = uuid();
+	@PepperField(key = "DocTime", store = Field.Store.YES, index = Field.Index.NOT_ANALYZED, type = EDataType.DATETIME)
+	private Date docTime = new Date();
+	@PepperField(key = "Year", store = Field.Store.YES, index = Field.Index.NOT_ANALYZED, type = EDataType.INTEGER)
+	private int year = year(getDocTime());
+	@PepperField(key = "Month", store = Field.Store.YES, index = Field.Index.NOT_ANALYZED, type = EDataType.INTEGER)
+	private int month = month(getDocTime());
+	@PepperField(key = "Day", store = Field.Store.YES, index = Field.Index.NOT_ANALYZED, type = EDataType.INTEGER)
+	private int day = day(getDocTime());
 
 	public String getUuid() {
 		return uuid;
@@ -24,6 +38,38 @@ public class BaseEntity implements Serializable {
 
 	public void setUuid(String uuid) {
 		this.uuid = uuid;
+	}
+
+	public Date getDocTime() {
+		return docTime;
+	}
+
+	public void setDocTime(Date docTime) {
+		this.docTime = docTime;
+	}
+
+	public int getYear() {
+		return year;
+	}
+
+	public void setYear(int year) {
+		this.year = year;
+	}
+
+	public int getMonth() {
+		return month;
+	}
+
+	public void setMonth(int month) {
+		this.month = month;
+	}
+
+	public int getDay() {
+		return day;
+	}
+
+	public void setDay(int day) {
+		this.day = day;
 	}
 
 }
--- pepper-spring/src/main/java/com/pepper/spring/service/IndexService.java
+++ pepper-spring/src/main/java/com/pepper/spring/service/IndexService.java
@@ -2,16 +2,26 @@ package com.pepper.spring.service;
 
 import javax.annotation.PostConstruct;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import com.pepper.lucene.Configuration;
+import com.pepper.lucene.PepperException;
 import com.pepper.lucene.analyzer.IAnalyzer;
 import com.pepper.lucene.analyzer.PaodingAnalyzer;
+import com.pepper.lucene.common.PepperResult;
+import com.pepper.lucene.comparator.base.PepperSortField;
+import com.pepper.lucene.comparator.base.PepperSortField.FieldType;
 import com.pepper.lucene.service.IndexDao;
 import com.pepper.spring.util.Resources;
 
+import static cn.com.lemon.base.Strings.isNullOrEmpty;
+import static cn.com.lemon.base.Preasserts.checkArgument;
+
 @Service
 public class IndexService {
+	private static final Logger LOG = LoggerFactory.getLogger(IndexService.class.getName());
 	private static final String RESOURCE_NAME = "pepper";
 	private IndexDao indexService;
 
@@ -29,4 +39,65 @@ public class IndexService {
 			resources.close();
 		}
 	}
+
+	/**
+	 * Search engine search service
+	 * <p>
+	 * search condition<blockquote>
+	 * 
+	 * <pre>
+	*     Title:China
+	 * </pre>
+	 * 
+	 * </blockquote>
+	 * <p>
+	 * 
+	 * @param queryString
+	 *            search condition
+	 * @param hits
+	 *            data sampling number
+	 * @param sortFields
+	 *            search result sort field
+	 * @param hightLightFields
+	 *            search result high light sort field
+	 * @param first
+	 *            paging first number
+	 * @param size
+	 *            paging size
+	 * @param path
+	 *            the index storage address
+	 * @return {@link PepperResult}
+	 */
+	public PepperResult search(String queryString, Integer hits, PepperSortField[] sortFields,
+			String[] hightLightFields, Integer first, Integer size, String... path) {
+		checkArgument(null != path && path.length > 0, "When data retrieval, index storage address is not empty!");
+		PepperResult result = new PepperResult();
+		// if query condition is null or empty,default search all
+		if (isNullOrEmpty(queryString))
+			queryString = "*:*";
+		/***/
+		LOG.debug("Query condition:[" + queryString + "]");
+		try {
+			if (path.length == 1) {
+				return indexService.search(path[0], queryString, hits, sortFields, hightLightFields, first, size);
+			} else {
+				return indexService.mSearch(path, queryString, hits, sortFields, hightLightFields, first, size);
+			}
+		} catch (PepperException e) {
+			LOG.error("There is an exception in search engine search!");
+			e.printStackTrace();
+			result.documents = null;
+			result.totalHits = 0;
+			return result;
+		}
+	}
+
+	public PepperResult search(String queryString, Integer first, Integer size, String... path) {
+		PepperSortField[] sortFields = { new PepperSortField("DocTime", FieldType.StringVal, true) };
+		// Default data sampling: 1 million
+		// Default data sort: date desc
+		return search(queryString, 1000000, sortFields, null, first, size, path);
+	}
+
+	
 }
